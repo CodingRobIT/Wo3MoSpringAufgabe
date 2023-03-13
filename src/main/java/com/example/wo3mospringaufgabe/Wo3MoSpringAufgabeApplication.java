a079ff50437895a183de822298b361d275d197ef
Robin Schatzl <tmai02476@gmail.com>
2023-03-13 16:31:53 +0100

    N/A

---
+++ src/main/java/com/example/wo3mospringaufgabe/Wo3MoSpringAufgabeApplication.java
@@ -0,0 +1,13 @@
+package com.example.wo3mospringaufgabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wo3MoSpringAufgabeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wo3MoSpringAufgabeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wo3mospringaufgabe/Wo3MoSpringAufgabeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wo3mospringaufgabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wo3MoSpringAufgabeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
